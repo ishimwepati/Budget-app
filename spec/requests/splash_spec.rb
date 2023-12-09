a7fa9a5a8890ac8ca4c3b3ec2cfa04f3bfb4ebfa
ishimwepati <wazacode@gmail.com>
2023-12-10 01:02:09 +0300

    Requested changes done

--- spec/requests/splash_spec.rb
+++ spec/requests/splash_spec.rb
@@ -1,7 +1,7 @@
-require 'rails_helper'
+#require 'rails_helper'
 
-RSpec.describe 'Splashes', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
-  end
-end
+#Spec.describe 'Splashes', type: :request do
+ # describe 'GET /index' do
+  #  pending "add some examples (or delete) #{__FILE__}"
+  #end
+#end
